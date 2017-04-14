5deb4deaac000ef4c1e0b7ece7827e940d7928ca
Sergei Gasanov <gasanov.sergei@gmail.com>
2017-04-14 15:06:28 +0300

    added History/Favorites activity

---
+++ app/src/main/java/com/electrologic/edutranslator/FragmentHistory.java
@@ -0,0 +1,20 @@
+package com.example.sergei.tabbedexample;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class FragmentHistory extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState)
+    {
+        View rootView = inflater.inflate(R.layout.fragment_history, container, false);
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/electrologic/edutranslator/HistoryFavoritesActivity.java
@@ -0,0 +1,13 @@
+package com.electrologic.edutranslator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HistoryFavoritesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history_favorites);
+    }
+}
